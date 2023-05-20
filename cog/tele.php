c1997513055f29801fc315d9fb091744a35dfffe
ibrahim <114075012+ibrahimgt94@users.noreply.github.com>
2023-05-20 06:36:49 +0200

    init

--- cog/tele.php
+++ cog/tele.php
@@ -1,15 +1,15 @@
 <?php
 
 \Face\Cog::add("tele", [
-  'title'     => "Rose Number Bot",
-  'robat'     => "@rosenum_bot",
-  'channel'   => "@rosenum_bot",
-  'admin'     => "5447525202",
-  "chatPri"   => "-1001319430114",
-  'supportId' => "@ibrahimgt94",
-  'token'     => "5845518915:AAF5U3UEniPAgR_wQuoHFt4M9veLOXJty28",
-  'group'     => "https://t.me/joinchat/EvES71PmhPg8JZsMRpxNpVQ",
-  'url'       => "https://rose1.ibr4him.ir",
+  'title'     => "Telegram bot virtual number",
+  'robat'     => "robat_id",
+  'channel'   => "channel_id",
+  'admin'     => "admin_ir",
+  "chatPri"   => "chatPri_id",
+  'supportId' => "supportId",
+  'token'     => "token",
+  'group'     => "--",
+  'url'       => "https://exampel.ir",
   'cons'      => 50,
   'status'    => false,
 ]);
